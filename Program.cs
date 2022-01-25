875868c2257def95ee8462797fe1f5bf12ad314d
James Brandao <muricijames@gmail.com>
2022-01-25 20:39:49 -0300

    Classe para testar estruturas

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EDC.Filas;
+
+namespace EDC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue Fila = new Queue();
+
+            Fila.Enqueue(1);
+            Fila.Enqueue(2);
+            Fila.Enqueue(3);
+
+            Fila.IsEmpty();
+
+            Fila.Front();
+
+            Console.WriteLine(Fila.Dequeue());
+
+            Fila.Show();
+
+            // Fila.Size();
+
+            // Fila.Dequeue();
+
+            // Fila.Size();
+
+        }
+    }
+}
