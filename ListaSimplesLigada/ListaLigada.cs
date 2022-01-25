b18887281ddcc7280aca0066425ca6dd74f19e26
James Brandao <muricijames@gmail.com>
2022-01-25 20:40:11 -0300

    Lista ligada

---
+++ ListaSimplesLigada/ListaLigada.cs
@@ -0,0 +1,15 @@
+namespace EDC.ListaSimplesLigada
+{
+    public class Node
+    {
+        public string Value;
+        public Node Next;
+    }
+
+    public class LinkedList
+    {
+        public int Length = 0;
+        public Node Node = null;
+    }
+
+}
