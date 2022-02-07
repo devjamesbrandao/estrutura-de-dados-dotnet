94a601443e1e8690fdde4e1261ec5ba59b513030
James Brandao <jamesbrandao@radinfo.com.br>
2022-02-07 07:20:02 -0300

    Dicionário e tabela hash

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using EDC.Filas;
 using EDC.ListaSimplesLigada;
 using estrutura_de_dados_dotnet.Conjuntos;
+using estrutura_de_dados_dotnet.Dicionarios;
+using estrutura_de_dados_dotnet.Hash;
 using estrutura_de_dados_dotnet.ListaDuplalLigada;
 
 namespace EDC_
@@ -10,8 +12,35 @@ namespace EDC_
     {
         static void Main(string[] args)
         {
-           Conjuntos();
+           TabelaHashe();
         }
+
+        static void TabelaHashe()
+        {
+            var hash = new TabelaHash();
+
+            hash.Adicionar("A", "Amora");
+
+            hash.Adicionar("B", "Buriti");
+        }
+
+        static void Dicio()
+        {
+            var dicio = new Dicionarios();
+
+            dicio.Set("A", "Abacaxi");
+
+            dicio.Set("B", "Banana");
+
+            dicio.Deletar("A");
+
+            foreach(var i in dicio.Valores())
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
 
         static void ListaDuplalLigada()
         {
@@ -36,13 +65,21 @@ namespace EDC_
 
             conjuntos.Adicionar("C");
 
-            var valores = conjuntos.Valores();
+            var conjuntos2 = new Conjuntos();
 
-            foreach(var i in valores)
+            conjuntos2.Adicionar("A");
+
+            conjuntos2.Adicionar("B");
+
+            conjuntos2.Adicionar("C");
+
+            foreach(var i in conjuntos.Diferenca(conjuntos2))
             {
                 Console.WriteLine(i);
             }
-        }
+
+            Console.WriteLine(conjuntos.SubConjunto(conjuntos2));
+        }  
 
         static void ListaLigada()
         {
